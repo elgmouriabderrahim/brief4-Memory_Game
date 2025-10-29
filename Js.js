5d525a712464f131e892a3f31cea8a2bb4ffa7dc
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-10-29 20:58:01 +0000

    add play again and start over buttons + styling the final msg

--- Js.js
+++ Js.js
@@ -1,160 +1,154 @@
 let myChoicebox = document.querySelector(".choicebox");
 let mycards = document.querySelector(".cards");
-thebody = document.querySelector("body")
+let thebody = document.querySelector("body");
+
 const cardImages = [
-    'images/card1.png',
-    'images/card2.png',
-    'images/card3.jpg',
-    'images/card4.jpg',
-    'images/card5.jpg',
-    'images/card6.jpg',
-    'images/card7.jpg',
-    'images/card8.jpg',
-    'images/card9.jpg',
-    'images/card10.jpg',
-    'images/card11.jpg',
-    'images/card12.jpg',
-    'images/card13.jpg',
-    'images/card14.jpg',
-    'images/card15.jpg',
-    'images/card16.jpg',
-    'images/card17.jpg',
-    'images/card18.jpg',
-    'images/card19.jpg',
-    'images/card20.jpg',
-    'images/card21.jpg',
-    'images/card22.jpg',
-    'images/card23.jpg',
-    'images/card24.jpg',
-    'images/card25.jpg',
-    'images/card26.jpg',
-    'images/card27.jpg',
-    'images/card28.jpg',
-    'images/card29.png',
-    'images/card30.webp',
-    'images/card31.png',
-    'images/card32.jpg',
+    'images/card1.png', 'images/card2.png', 'images/card3.jpg', 'images/card4.jpg',
+    'images/card5.jpg', 'images/card6.jpg', 'images/card7.jpg', 'images/card8.jpg',
+    'images/card9.jpg', 'images/card10.jpg', 'images/card11.jpg', 'images/card12.jpg',
+    'images/card13.jpg', 'images/card14.jpg', 'images/card15.jpg', 'images/card16.jpg',
+    'images/card17.jpg', 'images/card18.jpg', 'images/card19.jpg', 'images/card20.jpg',
+    'images/card21.jpg', 'images/card22.jpg', 'images/card23.jpg', 'images/card24.jpg',
+    'images/card25.jpg', 'images/card26.jpg', 'images/card27.jpg', 'images/card28.jpg',
+    'images/card29.png', 'images/card30.webp', 'images/card31.png', 'images/card32.jpg',
 ];
 
 document.getElementById("valider").addEventListener("click", () => {
     const myChoice = document.querySelector("input[name='choice']:checked");
-    if (myChoice){
+    if (myChoice) {
         mycards.style.display = "grid";
         myChoicebox.style.display = "none";
-    
         mycards.innerHTML = '';
         mycards.className = "cards";
-    
         mycards.classList.add(`matrix${myChoice.value}x${myChoice.value}`);
-    
+
         let gametitle = document.createElement("span");
         gametitle.textContent = `MEMORY GAME ${myChoice.value} X ${myChoice.value}`;
         gametitle.style.gridColumn = "1 / -1";
         mycards.appendChild(gametitle);
-    
+
         let totalCards = myChoice.value * myChoice.value;
-    
+
         let selectedImages = cardImages.slice(0, totalCards / 2);
         let cardsPool = [...selectedImages, ...selectedImages];
-    
+
         function shuffle(array) {
-            let temp;
-            for (let i = 0 ; i < array.length; i++) {
+            for (let i = 0; i < array.length; i++) {
                 let j = Math.floor(Math.random() * (i + 1));
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                [array[i], array[j]] = [array[j], array[i]];
             }
             return array;
         }
+
         cardsPool = shuffle(cardsPool);
-        
+
         cardsPool.forEach(imgSrc => {
             const card = document.createElement("div");
             let temp = imgSrc.split("/")[1].split(".")[0];
             card.setAttribute("data-name", temp);
             card.classList.add("card");
+
             if (myChoice.value == 2) {
                 card.style.width = "200px";
                 card.style.height = "300px";
                 mycards.style.rowGap = "20px";
-            }
-            else if (myChoice.value == 4){
+            } else if (myChoice.value == 4) {
                 card.style.width = "100px";
                 card.style.height = "150px";
                 mycards.style.rowGap = "12px";
                 mycards.style.columnGap = "30px";
-            }
-            else if (myChoice.value == 6){
+            } else if (myChoice.value == 6) {
                 card.style.width = "60px";
                 card.style.height = "90px";
                 mycards.style.rowGap = "10px";
-            }
-            else if (myChoice.value == 8){
+            } else if (myChoice.value == 8) {
                 card.style.width = "50px";
                 card.style.height = "75px";
                 mycards.style.rowGap = "5px";
             }
+
             const back = document.createElement("img");
             back.src = "images/cardback.jpg";
             back.classList.add("back");
-    
+
             const front = document.createElement("img");
             front.src = imgSrc;
             front.classList.add("front");
-    
+
             card.appendChild(back);
             card.appendChild(front);
             mycards.appendChild(card);
-            
+
             card.addEventListener("click", function () {
-                if(!(this.classList.contains("decorate"))){
+                if (!this.classList.contains("decorate")) {
                     this.classList.toggle("flip");
                 }
             });
         });
-            let cards = document.querySelectorAll(".card");
-            let flipped = 'false';
-            let temp;
-            let count = 0;
-            let nbrflipped = 0;
-            let bestScore;
-            cards.forEach(currentCard => {
-                currentCard.addEventListener("click", () => {
-                    count++;
-                    if(flipped == 'false'){
-                        flipped = 'true';
-                        temp = currentCard;
-                    }else{
-                        
-                        if(temp.getAttribute("data-name") === currentCard.getAttribute("data-name") && currentCard !== temp){
-                            setTimeout(() =>{
-                                currentCard.classList.add("decorate");
-                                temp.classList.add("decorate");
-                            }, 500)
-                            nbrflipped = nbrflipped + 2;
-                        }else{
-                           setTimeout(() => {
+
+        let cards = document.querySelectorAll(".card");
+        let flipped = 'false';
+        let temp;
+        let count = 0;
+        let nbrflipped = 0;
+        let bestScore;
+
+        cards.forEach(currentCard => {
+            currentCard.addEventListener("click", () => {
+                count++;
+                if (flipped == 'false') {
+                    flipped = 'true';
+                    temp = currentCard;
+                } else {
+                    if (temp.getAttribute("data-name") === currentCard.getAttribute("data-name") && currentCard !== temp) {
+                        setTimeout(() => {
+                            currentCard.classList.add("decorate");
+                            temp.classList.add("decorate");
+                        }, 500);
+                        nbrflipped += 2;
+                    } else {
+                        setTimeout(() => {
                             currentCard.classList.toggle("flip");
                             temp.classList.toggle("flip");
-                           }, 500);
-                        }
-                        flipped = 'false';
+                        }, 500);
                     }
-                    if(nbrflipped == totalCards){
-                        mycards.style.display = "none";
-                        let finalMsg = document.createElement("div");
-                        finalMsg.innerText = `Congratulations! You won with ${count} attempts`;
-                        finalMsg.classList.add("congratestyle");
-                        thebody.append(finalMsg);
+                    flipped = 'false';
+                }
 
-                        bestScore = bestScore ? Math.min(bestScore, count) : count;
+                if (nbrflipped == totalCards) {
+                    mycards.style.display = "none";
+                    let finalMsg = document.createElement("div");
+                    finalMsg.innerText = `Congratulations! You won with ${count} attempts`;
+                    finalMsg.classList.add("congratestyle");
+                    thebody.append(finalMsg);
 
-                        let bestscorediv = document.createElement("p");
-                        bestscorediv.innerText = `The best score so far: ${bestScore}`;
-                        finalMsg.append(bestscorediv);
-                    }
-                })
-            })
-        }
+                    bestScore = bestScore ? Math.min(bestScore, count) : count;
+
+                    let bestscorediv = document.createElement("p");
+                    bestscorediv.innerText = `The best score so far: ${bestScore}`;
+                    finalMsg.append(bestscorediv);
+
+                    let startoverbtn = document.createElement("button");
+                    startoverbtn.innerText = "Start Over";
+                    startoverbtn.addEventListener("click", () => location.reload());
+                    finalMsg.append(startoverbtn);
+
+                    let playagainbtn = document.createElement("button");
+                    playagainbtn.innerText = "Play Again";
+                    playagainbtn.addEventListener("click", () => {
+                        finalMsg.remove();
+                        nbrflipped = 0;
+                        count = 0;
+                        mycards.style.display = "grid";
+                        document.querySelectorAll(".card").forEach(current => {
+                            current.classList.remove("flip", "decorate");
+                        });
+                    });
+                    finalMsg.append(playagainbtn);
+                    playagainbtn.classList.add("button-decoraation");
+                    startoverbtn.classList.add("button-decoraation");
+                }
+            });
+        });
+    }
 });
